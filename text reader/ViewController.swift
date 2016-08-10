a73a1a06c0ccab0e16149c31da7a01ce413602af
Vincenthng <vincent_gameboy@hotmail.com>
2016-08-10 08:53:45 +0800

    Initial Commit

---
+++ text reader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  text reader
+//
+//  Created by Vincenthng on 16/8/10.
+//  Copyright © 2016年 Vincenthng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
